6ff45aecd6a0024294691f8be1a12fca00b24deb
GokulVelavan <gokulvelavans@gmail.com>
2021-11-29 19:32:43 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianSensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian census analyser");
+        }
+    }
+}
